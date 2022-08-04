2942e8c8240e2e3a44a95e3cdcbd92be1bbe0f1f
Utkarshmaury <94758217+Utkarshmaury@users.noreply.github.com>
2022-08-04 15:11:19 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 clear
-BRed='\033[1;31m'
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
 printf "${BRed}"
 echo
 echo	" __   __  _______    ______   _______  _     _  __    _ "
 echo	"|  | |  ||       |  |      | |       || | _ | ||  |  | |"
-echo	"|  |_|  ||_     _|  |  _    ||   _   || || || ||   |_| |"
+echo	"|  |_|  ||_     _|  |       ||   _   || || || ||   |_| |"
 echo	"|       |  |   |    | | |   ||  | |  ||       ||       |"
 echo	"|_     _|  |   |    | |_|   ||  |_|  ||       ||  _    |"
 echo	"  |   |    |   |    |       ||       ||   _   || | |   |"
 echo    "  |___|    |___|    |______| |_______||__| |__||_|  |__|"
 echo "CREATED BY UTKARSH BRO"
 printf "\n"
-python3  ytdown.py
+printf "${BGreen} Paste Youtube link Here:\n"
+echo link:-
+read link
+cd /sdcard
+pytube "$link"  
+cd $HOME
+printf "${BBlue}Video has been Downloaded into /sdcard successfully."
+
