79ab49f79963c730945df1e51dce37a30efc9ddb
Utkarshmaury <94758217+Utkarshmaury@users.noreply.github.com>
2022-08-04 15:17:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-pkg upgrade
-pkg update
-pkg install python3
+apt upgrade
+apt update
+apt install python3
 pip install -r requirments.txt
 echo "run ./setup.sh to launch script."
 chmod +x setup.sh
